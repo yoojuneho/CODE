5ba8abc1a799e4de6abb983acce44c59930b5ffd
juno <yoojuneho0723@naver.com>
2024-01-17 16:26:32 +0900

    1000번문제

---
+++ 백준/no.1000.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int add(){
+  int a, b; // scanf를 사용할 때 변수 초기화
+  scanf("%d", &a);
+  scanf("%d", &b);
+  if(a > 0 && b < 10){
+    int sum = a + b;
+    return sum; // 반환해야 함
+  } else {
+    return 1;
+  }
+}
+
+int main(void){
+  int result = add(); // add함수의 리턴타입이 int
+  printf("%d", result); 
+  return 0;
+}
