d3e8cdbee1e88cadd8704b98b93a2dc38364773c
yoojuneho <yoojuneho0723@naver.com>
2024-03-14 14:56:36 +0900

    문자열 처리

---
+++ 파이썬문법공부/string_manupulation.py
@@ -0,0 +1,20 @@
+snack = "꿀꽈배기"
+two = " 2개"
+juseyo  = snack + two
+print(juseyo)
+
+#이런식으로 붙여도 됨
+juseyo += " 주세요"
+print(juseyo)
+
+#문자열 길이 구하기, len
+print("-------------문자열 길이 구하기, len-------------")
+print(len(snack))
+
+
+#여러줄로 출력할 때 ``` ``` 사용
+print("-------------여러줄로 출력할 때 ``` ``` 사용-------------")
+yumi = '''꿀꽈배기
+너무 
+맛있어요'''
+print(yumi)
