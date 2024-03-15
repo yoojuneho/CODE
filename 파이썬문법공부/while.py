b167633dc2b7b2967461147bd7ec0cf7cf461e70
yoojuneho <yoojuneho0723@naver.com>
2024-03-15 16:52:54 +0900

    break, continue 활용

--- 파이썬문법공부/while.py
+++ 파이썬문법공부/while.py
@@ -13,3 +13,18 @@ while weight + item <= max:
     weight += item
     print('짐을 추가합니다.')
 print(f'총 무게는 {weight}입니다.')
+
+#break
+drama = ['시즌1', '시즌2', '시즌3', '시즌4', '시즌5']
+for x in drama:
+    if x == '시즌3':
+        print('재미 없대, 그만 보자')
+        break
+    print(f'{x} 시청')
+
+#continue
+for x in drama:
+    if x == '시즌3':
+        print('재미 없대, 건너 뛰자')
+        continue
+    print(f'{x} 시청')
